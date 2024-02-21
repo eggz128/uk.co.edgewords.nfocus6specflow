a27bfbe79d849cb2d9042bdcfce287530211204c
Steve Powell <steve.powell@edgewords.co.uk>
2024-02-21 13:25:39 +0000

    Add project files.

---
+++ uk.co.edgewords.nfocus6specflow/Features/GoogleSearch.feature
@@ -0,0 +1,9 @@
+﻿Feature: GoogleSearch
+In order to get more business
+Tom (CEO) wants
+us to appear high in testing search results
+
+Scenario: Search Google for Edgewords
+	Given I am on the Google homepage
+	When I search for 'BBC'
+	Then 'Edgewords' is the top result
---
+++ uk.co.edgewords.nfocus6specflow/StepDefinitions/GoogleSearchStepDefinitions.cs
@@ -0,0 +1,37 @@
+//Class generated with Specflow extension
+//Does not include ScenarioContext by default for sharing state between steps
+
+//using System;
+//using TechTalk.SpecFlow;
+
+//namespace uk.co.edgewords.nfocus6specflow.StepDefinitions
+//{
+//    [Binding]
+//    public class GoogleSearchStepDefinitions
+//    {
+//        private readonly ScenarioContext _scenarioContext;
+
+//        public GoogleSearchStepDefinitions(ScenarioContext sc)
+//        {
+//            _scenarioContext = sc;
+//        }
+
+//        [Given(@"I am on the Google homepage")]
+//        public void GivenIAmOnTheGoogleHomepage()
+//        {
+//            throw new PendingStepException();
+//        }
+
+//        [When(@"I search for Edgewords")]
+//        public void WhenISearchForEdgewords()
+//        {
+//            throw new PendingStepException();
+//        }
+
+//        [Then(@"Edgewords is the top result")]
+//        public void ThenEdgewordsIsTheTopResult()
+//        {
+//            throw new PendingStepException();
+//        }
+//    }
+//}
